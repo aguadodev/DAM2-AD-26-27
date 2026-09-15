b712429ae9324e8b01ba71224532b8b53b3ee432
aguadodev <aguado@edu.xunta.es>
2026-09-15 12:51:22 +0200

    Ejemplo Flujo binario

---
+++ src/EjemploCopiaArchivos.java
@@ -0,0 +1,28 @@
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+
+public class EjemploCopiaArchivos {
+
+
+   public static void main(String[] args) throws IOException {
+       FileInputStream in = null;
+       FileOutputStream out = null;
+       try {
+           in = new FileInputStream("README.md");
+           out = new FileOutputStream("destino.txt");
+           int c;
+           while ((c = in.read()) != -1) {
+               out.write(c);
+           }
+       } finally { // Hay que cerrar el flujo en cualquier condición.
+           if (in != null) {
+               in.close();
+           }
+           if (out != null) {
+               out.close();
+           }
+       }
+   }
+}
