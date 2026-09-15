c84b918edcd0ed91e8a6ecf6ffe60879e2626c3e
aguadodev <aguado@edu.xunta.es>
2026-09-15 12:34:09 +0200

    Ejercicio2NIO

--- src/Ejercicio2NIO.java
+++ src/Ejercicio2NIO.java
@@ -1,3 +1,4 @@
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -8,10 +9,16 @@ import javax.swing.JFileChooser;
 
 public class Ejercicio2NIO {
 
+    static long totalSize = 0;
+
     public static void imprime(Path p) {
         System.out.print("Nombre: " + p.getFileName());
         try {
-            System.out.println(" (" + Files.size(p) + ")");
+            long size = Files.size(p);
+            totalSize += size;
+            System.out.print(" (" + size + ")");
+            System.out.println(Files.isDirectory(p) ? " DIRECTORIO" : "");
+
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -25,6 +32,9 @@ public class Ejercicio2NIO {
             // Try-catch con recursos (cierra automáticamente el flujo)
             try (Stream<Path> stream = Files.list(path)) {
                 stream.forEach(Ejercicio2NIO::imprime);
+
+                System.out.println("Tamaño Total: " + totalSize + " bytes");
+
             } catch (IOException e) {
                 e.printStackTrace();
             }
