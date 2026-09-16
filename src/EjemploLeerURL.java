c8f19b1d29aaf2b2d90732724582c3f501a7dcf4
aguadodev <aguadoaudiovisual@gmail.com>
2026-09-16 09:53:02 +0200

    Ejemplo LeerURL

---
+++ src/EjemploLeerURL.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.net.URI;
+import java.net.URL;
+
+public class EjemploLeerURL {
+    public static void main(String[] args) throws Exception {
+        URI uri = new URI("https://data.iana.org/TLD/tlds-alpha-by-domain.txt");
+        URL url = uri.toURL();
+        InputStream is = url.openStream();
+        InputStreamReader isr = new InputStreamReader(is);
+        // es un puente de bytes a caracteres.
+        int c;
+        while ((c = isr.read()) != -1) {
+            System.out.print((char) c);
+        }
+     }
+//        // Código equivalente con buffer:
+//        try (InputStream is = url.openStream();
+//             InputStreamReader isr = new InputStreamReader(is);
+//             BufferedReader br = new BufferedReader(isr)) { // Lo veremos en el siguiente apartado.
+//            String line;
+//            while ((line = br.readLine()) != null) {
+//                System.out.println(line);
+//            }
+//        }
+    
+}
