105b24d87aa7e2656643cf0e402f27e817b80422
aguadodev <aguado@edu.xunta.es>
2026-09-18 13:49:02 +0200

    Serialización 1

---
+++ src/EjemploFlujoSerializado.java
@@ -0,0 +1,30 @@
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.time.LocalDate;
+
+public class EjemploFlujoSerializado {
+    public static void main(String[] args) {
+        Persona persona1 = new Persona("Pepe", LocalDate.of(2000, 1, 1), "abc123.,");
+        Persona persona2 = new Persona("Marta", LocalDate.of(2001, 2, 2), "12345");
+
+        // Escribir objetos serializados
+        try (var out = new ObjectOutputStream(new FileOutputStream("personasSerie.dat"))) {
+            out.writeObject(persona1);
+            out.writeObject(persona2);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // Leer Objetos serializados
+        try (var in = new ObjectInputStream(new FileInputStream("personasSerie.dat"))) {
+            Persona persona3 = (Persona) in.readObject();
+            Persona persona4 = (Persona) in.readObject();
+            System.out.println(persona3);
+            System.out.println(persona4);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/Persona.java
@@ -0,0 +1,24 @@
+import java.io.Serializable;
+import java.time.LocalDate;
+
+public class Persona implements Serializable{
+    private static final long serialVersionUID = 1L;
+
+    private String nombre;
+    private LocalDate fechaNacimiento;
+    private transient String contrasenia; // no se serializa
+
+    public Persona(String nombre, LocalDate fechaNacimiento, String contrasenia) {
+        this.nombre = nombre;
+        this.fechaNacimiento = fechaNacimiento;
+        this.contrasenia = contrasenia;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona [nombre=" + nombre + ", fechaNacimiento=" + fechaNacimiento + ", contrasenia=" + contrasenia
+                + "]";
+    }
+
+    
+}
