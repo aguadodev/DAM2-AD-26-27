4a2d97b49f99a6453363cbeb14ac8b473e7cd057
aguadodev <aguado@edu.xunta.es>
2026-09-21 15:16:27 +0200

    Firmas

--- src/ud1/FirmasHexadecimales.java
+++ src/ud1/FirmasHexadecimales.java
@@ -3,35 +3,55 @@ package ud1;
 import java.io.FileInputStream;
 import java.io.IOException;
 
-/**
- * @author Eusebio Moreira Dominguez
- */
+
 public class FirmasHexadecimales {
+
+    // PROBLEMAS
+
+    /* Como obtener la extensión de un archivo (si tiene) */
+
+    /* Formas de almacenar los pares extensión/firma
+       1. Matriz de String
+       2. Clase Extension {extensión, firma, trailer, etc...}
+       3. Map<String, String>
+    */
+
+    /* Como convertir dos caracteres hexadecimales en formato String a formato byte/int */       
+
+    
     public static void main(String[] args) {
-        verificarFirma("Programación DAM2-AD-26-27.pdf");
-        verificarFirma("imagen");
-        verificarFirma("foto.jpg");
+        firmaVerificada("Programación DAM2-AD-26-27.pdf");
+        firmaVerificada("imagen");
+        firmaVerificada("foto.jpg");
     }
 
-    public static void verificarFirma(String ruta) {
+    public static boolean firmaVerificada(String ruta) {
 
-        if (ruta.contains(".")) {
-            String[] partes = ruta.split(".");
+        if (ruta != null && ruta.contains(".")) {
+            String[] partes = ruta.split("\\.");
             String extension = partes[partes.length - 1].toUpperCase();
 
             try (FileInputStream bf = new FileInputStream(ruta)) {
-                int b1 = bf.read();
-                int b2 = bf.read();
-                int b3 = bf.read();
-                int b4 = bf.read();
+                int b1 = 0;
+                int b2 = 0;
+                int b3 = 0;
+                int b4 = 0;
 
                 boolean coincide = false;
 
                 switch (extension) {
                     case "PDF":
-                        if (b1 == 0x25 && b2 == 0x50 && b3 == 0x44 && b4 == 0x46) {
-                            coincide = true;
+                        coincide = true;
+                        String firma = "25 50 44 46"; // Con espacios o sin espacios?
+                        String[] bytesStr = firma.split(" ");
+                        for (String byteStr : bytesStr) {
+                            // int b = Integer.valueOf(byteStr, 16);
+                            if (bf.read() != Integer.valueOf(byteStr, 16))
+                                coincide = false;
                         }
+                        /*if (b1 == 0x25 && b2 == 0x50 && b3 == 0x44 && b4 == 0x46) {
+                            coincide = true;
+                        }*/
                         break;
                     case "PNG":
                         if (b1 == 0x89 && b2 == 0x50 && b3 == 0x4E && b4 == 0x47) {
